a0cba8b2f3b94f9a35b1bf7b626e0ce69cfadbbd
Gergő Albert Zemanek <Zemanek.Gergo.Albert.2017F@gdszeged.onmicrosoft.com>
2022-04-06 16:12:57 +0200

    getterek és setterek készen

--- RectangleProject/RectangleProject/Program.cs
+++ RectangleProject/RectangleProject/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace RectangleProject
+﻿namespace RectangleProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
---
+++ RectangleProject/RectangleProject/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿namespace RectangleProject
+{
+    class Rectangle
+    {
+        private double sideA;
+        private double sideB;
+
+        public double SideA
+        {
+            get
+            {
+                return sideA;
+            }
+            set
+            {
+                SideA = value;
+            }
+        }
+
+        public double SideB
+        {
+            get
+            {
+                return sideB;
+            }
+            set
+            {
+                SideB = value;
+            }
+        }
+
+        public Rectangle(double sideA, double sideB)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+    }
+}
